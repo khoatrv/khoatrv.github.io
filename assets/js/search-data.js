49eb9cdfd56cd8f3b8fdd7135760e205451ca886
khoatrv <135490565+khoatrv@users.noreply.github.com>
2026-06-13 01:58:15 +0000

    Deploying to gh-pages from @ khoatrv/khoatrv.github.io@166075185440fab0f9b29963150d1fe130d80814 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
